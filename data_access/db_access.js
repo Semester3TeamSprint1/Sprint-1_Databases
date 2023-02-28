7616be0e6c21fb3e7bd764d1293fd1dd9e298139
Darla Ward <105027556+darlamward@users.noreply.github.com>
2023-02-28 14:12:34 -0330

    Some comments to explain the functions.

--- data_access/db_access.js
+++ data_access/db_access.js
@@ -1,91 +1,111 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'QAP1',
-  password: 'cupcake',
+  user: "postgres",
+  host: "localhost",
+  database: "QAP1",
+  password: "cupcake",
   port: 5432,
-})
+});
 
+// Gets data for cities.
 const getCities = (request, response) => {
-    pool.query('SELECT * FROM city ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
+  pool.query("SELECT * FROM city ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
+// Gets data for passengers.
 const getPassengers = (request, response) => {
-pool.query('SELECT * FROM passenger ORDER BY id ASC', (error, results) => {
+  pool.query("SELECT * FROM passenger ORDER BY id ASC", (error, results) => {
     if (error) {
-    throw error
+      throw error;
     }
-    response.status(200).json(results.rows)
-})
-}
+    response.status(200).json(results.rows);
+  });
+};
 
+// Gets data for airports.
 const getAirports = (request, response) => {
-pool.query('SELECT * FROM airport ORDER BY id ASC', (error, results) => {
+  pool.query("SELECT * FROM airport ORDER BY id ASC", (error, results) => {
     if (error) {
-    throw error
+      throw error;
     }
-    response.status(200).json(results.rows)
-})
-}
+    response.status(200).json(results.rows);
+  });
+};
 
+// Gets data for aircrafts.
 const getAircrafts = (request, response) => {
-pool.query('SELECT * FROM aircraft ORDER BY id ASC', (error, results) => {
+  pool.query("SELECT * FROM aircraft ORDER BY id ASC", (error, results) => {
     if (error) {
-    throw error
+      throw error;
     }
-    response.status(200).json(results.rows)
-})
-}
+    response.status(200).json(results.rows);
+  });
+};
 
+// Gets data to show what AIRPORTS are in what CITIES.
 const getCityAirport = (request, response) => {
-    pool.query('select * from airport a, city c where a.city_id = c.id order by c.city_state;', (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
+  pool.query(
+    "select * from airport a, city c where a.city_id = c.id order by c.city_state;",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     }
+  );
+};
 
+// Gets data for a list of AIRCRAFTS that PASSENGERS have travelled on.
 const getPassengerAircraft = (request, response) => {
-    pool.query('select p.first_name, p.last_name, a.aircraft_type, a.airline_name from passenger p, aircraft a, passenger_aircraft pa where pa.passenger_id = p.id and pa.aircraft_id = a.id order by p.last_name;', (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
+  pool.query(
+    "select p.first_name, p.last_name, a.aircraft_type, a.airline_name from passenger p, aircraft a, passenger_aircraft pa where pa.passenger_id = p.id and pa.aircraft_id = a.id order by p.last_name;",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     }
+  );
+};
 
+// Gets data for what AIRPORTS can AIRCRAFTS take off and land at.
 const getAirportAircraft = (request, response) => {
-    pool.query('select c.city_name as depart_city, c.city_state as depart_state, ap.airport_name as depart_airport, ap.airport_code as depart_code, ac.aircraft_type, ar.city_name as arrival_city, ar.city_state as arrival_state from airport ap, aircraft ac, city c, arrival_city ar, flight f where ap.id = f.airport_id and ac.id = f.aircraft_id and c.id = f.depart_id and ar.id = f.arrive_id order by ap.airport_name;', (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
+  pool.query(
+    "select c.city_name as depart_city, c.city_state as depart_state, ap.airport_name as depart_airport, ap.airport_code as depart_code, ac.aircraft_type, ar.city_name as arrival_city, ar.city_state as arrival_state from airport ap, aircraft ac, city c, arrival_city ar, flight f where ap.id = f.airport_id and ac.id = f.aircraft_id and c.id = f.depart_id and ar.id = f.arrive_id order by ap.airport_name;",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     }
+  );
+};
 
+// Gets data for what AIRPORTS have PASSENGERS used.
 const getAirportPassenger = (request, response) => {
-    pool.query('select p.first_name, p.last_name, a.airport_name, a.airport_code from passenger p, airport a, passenger_airport pa where pa.passenger_id = p.id and pa.airport_id = a.id order by p.last_name;', (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
+  pool.query(
+    "select p.first_name, p.last_name, a.airport_name, a.airport_code from passenger p, airport a, passenger_airport pa where pa.passenger_id = p.id and pa.airport_id = a.id order by p.last_name;",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     }
-    
+  );
+};
+
 module.exports = {
-    getCities,
-    getPassengers,
-    getAirports,
-    getAircrafts,
-    getCityAirport,
-    getPassengerAircraft,
-    getAirportAircraft,
-    getAirportPassenger
-}
+  getCities,
+  getPassengers,
+  getAirports,
+  getAircrafts,
+  getCityAirport,
+  getPassengerAircraft,
+  getAirportAircraft,
+  getAirportPassenger,
+};
--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const dbAccess = require('./data_access/db_access')
-const port = 3000
+// Sprint 1 - Database Programming. Using QAP 1 data tables created in pgAdmin to produce data using JavaScript.
+// Completed by: Darla Ward, Danielle Reid and Jarod Chambers-Genge
+// Completed on 02/28/2023
 
-app.use(bodyParser.json())
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const dbAccess = require("./data_access/db_access");
+const port = 3000;
+
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
-app.get('/cities', dbAccess.getCities)
-app.get('/passengers', dbAccess.getPassengers)
-app.get('/airports', dbAccess.getAirports)
-app.get('/aircrafts', dbAccess.getAircrafts)
-app.get('/city_airport', dbAccess.getCityAirport)
-app.get('/passenger_aircraft', dbAccess.getPassengerAircraft)
-app.get('/airport_aircraft', dbAccess.getAirportAircraft)
-app.get('/airport_passenger', dbAccess.getAirportPassenger)
+// Shows the data when navigating to the pages.
+app.get("/cities", dbAccess.getCities);
+app.get("/passengers", dbAccess.getPassengers);
+app.get("/airports", dbAccess.getAirports);
+app.get("/aircrafts", dbAccess.getAircrafts);
+app.get("/city_airport", dbAccess.getCityAirport);
+app.get("/passenger_aircraft", dbAccess.getPassengerAircraft);
+app.get("/airport_aircraft", dbAccess.getAirportAircraft);
+app.get("/airport_passenger", dbAccess.getAirportPassenger);
 
 // Express Server Start
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+  console.log(`App running on port ${port}.`);
+});
